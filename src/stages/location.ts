7892ebefad3c36c0bc4ffe542c986ef54a15ba19
Solant <runner62v6@gmail.com>
2019-08-25 02:12:33 +0300

    Implemented travel

--- src/stages/location.ts
+++ src/stages/location.ts
@@ -2,6 +2,8 @@ import BaseScene from 'telegraf/scenes/base';
 import { getRepository } from 'typeorm';
 
 import { User } from '../models/User';
+import { Location } from '../models/Location';
+import { logger } from '../logger';
 
 export enum LocationScenes {
     Intro = 'intro',
@@ -21,11 +23,44 @@ enter.enter(async ctx => {
         });
 });
 
-enter.on('callback_query', async ctx => {
-    await ctx.reply('asd');
-    await ctx.answerCbQuery('');
+enter.on('text', ctx => ctx.scene.reenter());
+
+enter.on('callback_query', async (ctx, next) => {
+    if (ctx.callbackQuery!.data === 'travel') {
+        const locations = await getRepository(Location).find();
+        const user = await getRepository(User)
+            .findOneOrFail({ where: { chatId: ctx.chat!.id }, relations: ['location'] });
+
+        const availableLocations = locations.filter(l => user.location.id !== l.id);
+        const buttons = availableLocations.map(l => ([{ text: l.name, callback_data: `travel-to:${l.id}` }]));
+        await ctx.reply('Список доступных локаций:', {
+            reply_markup: {
+                inline_keyboard: buttons,
+            },
+        });
+        await ctx.answerCbQuery();
+    }
+    if (next) next();
 });
 
+enter.on('callback_query', async (ctx, next) => {
+    if (ctx.callbackQuery!.data!.startsWith('travel-to')) {
+        const id = ctx.callbackQuery!.data!.split(':')[1];
+        const location = await getRepository(Location).findOne(id);
+        if (!location) {
+            logger.error(`Unable to find location with id=${id}`);
+            await ctx.reply('Невозможно отправиться в эту локацию');
+        } else {
+            setTimeout(() => {
+                ctx.reply('Прибыли');
+                getRepository(User).update({ chatId: ctx.chat!.id }, { location });
+            }, 10000);
+            await ctx.reply('Вылетаем, прибытие через 10 секунд');
+        }
+        await ctx.answerCbQuery();
+    }
+    if (next) next();
+});
 
 
 export default [enter];
